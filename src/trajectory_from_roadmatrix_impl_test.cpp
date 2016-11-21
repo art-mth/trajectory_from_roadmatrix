1ee9e54a8639b93de040048797512f9ed8179261
Arthur Mathies <arthur.mathies@googlemail.com>
2016-11-21 09:56:49 +0100

    Fix tests

--- src/trajectory_from_roadmatrix_impl_test.cpp
+++ src/trajectory_from_roadmatrix_impl_test.cpp
@@ -1,3 +1,13 @@
+/**
+ * Use this file for quick test prototyping as an alternative to logging. This
+ * module is bound to change and it is hard to write ever lasting tests for it.
+ * When there are tests that are not compiling anymore look at them and make
+ * sure you know what the tests idea was. If you understand the tests purpose
+ * and are sure your change did not break this constraint, but it is just a
+ * value problem delete it or write a new test. Use this to make sure your code
+ * is doing what you are expecting it to do.
+ */
+
 #include "trajectory_from_roadmatrix_impl.h"
 
 #include <vector>
@@ -22,7 +32,10 @@ class TrajectoryFromRoadMatrixTest : public testing::Test {
           cellsPerLane(4),
           cellLength(0.1),
           carWidthMeter(0.2),
-          obstacleClearanceMeter(0.5) {
+          obstacleClearanceLeftFrontMeter(1.0),
+          obstacleClearanceRightFrontMeter(0.5),
+          obstacleClearanceLeftBackMeter(0.5),
+          obstacleClearanceRightBackMeter(0.5) {
         polyLine2f straightLine;
         const int lineLength = 20;
         for (int i = 0; i < lineLength; i++) {
@@ -33,6 +46,10 @@ class TrajectoryFromRoadMatrixTest : public testing::Test {
                               cellLength);
 
         trajectory_creator.setCarWidthMeter(carWidthMeter);
+        trajectory_creator.setObstacleClearanceLeftFrontMeter(obstacleClearanceLeftFrontMeter);
+        trajectory_creator.setObstacleClearanceRightFrontMeter(obstacleClearanceRightFrontMeter);
+        trajectory_creator.setObstacleClearanceLeftBackMeter(obstacleClearanceLeftBackMeter);
+        trajectory_creator.setObstacleClearanceRightBackMeter(obstacleClearanceRightBackMeter);
         trajectory_creator.calculateCycleConstants(roadMatrix);
     }
 
@@ -44,23 +61,26 @@ class TrajectoryFromRoadMatrixTest : public testing::Test {
     const int cellsPerLane;
     const float cellLength;
     const float carWidthMeter;
-    const float obstacleClearanceMeter;
+    const float obstacleClearanceLeftFrontMeter;
+    const float obstacleClearanceRightFrontMeter;
+    const float obstacleClearanceLeftBackMeter;
+    const float obstacleClearanceRightBackMeter;
 };
 
-TEST_F(TrajectoryFromRoadMatrixTest, valueFunction) {
+TEST_F(TrajectoryFromRoadMatrixTest, createLanePieceMatrixValuesScaled) {
     std::vector<EnvironmentObjectPtr> obstacles;
-
     street_environment::ObstaclePtr obst1(new street_environment::Obstacle());
     obst1->addPoint(lms::math::vertex2f(0.55, 0.05));
+    obst1->addPoint(lms::math::vertex2f(0.55, -0.05));
     obstacles.push_back(obst1);
 
     roadMatrix.markEnvironmentObjects(obstacles);
 
-    EXPECT_EQ(
-        trajectory_creator.valueFunction(roadMatrix.cell(0, 0), roadMatrix),
-        12);
-    EXPECT_EQ(
-        trajectory_creator.valueFunction(roadMatrix.cell(0, 5), roadMatrix), 5);
+    std::unique_ptr<LanePieceMatrix> lanePieceMatrix =
+        trajectory_creator.createLanePieceMatrix(roadMatrix);
+    EXPECT_GT(lanePieceMatrix->at(4).at(5).value, lanePieceMatrix->at(4).at(4).value);
+    EXPECT_GE(lanePieceMatrix->at(4).at(5).value, lanePieceMatrix->at(4).at(6).value);
+    EXPECT_LT(lanePieceMatrix->at(4).at(5).value, lanePieceMatrix->at(4).at(3).value);
 }
 
 TEST_F(TrajectoryFromRoadMatrixTest, getOptimalLanePieceTrajectoryBlockedRoad) {
